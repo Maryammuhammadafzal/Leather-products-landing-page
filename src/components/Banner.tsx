778faa0748b9a6ee3753cbc62670676c46dde700
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-03 20:13:59 +0500

    6 section created

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className='w-full h-[450px] py-10 flex justify-center items-center'>
       <Image
-              src="/images/banner-image.jpg"
+              src="/images/banner-image.png"
               alt="image"
               width={400}
               height={400}
